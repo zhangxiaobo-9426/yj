da07169710968ce397bf9440d122d5418feeaf3d
15507 <1550751448@qq.com>
2021-01-18 16:44:55 +0800

    bottomNavigationView
    底部导航、去掉系统顶部导航、修改系统顶部颜色

---
+++ app/src/main/java/com/example/yj/ui/accompany/AccompanyFragment.java
@@ -0,0 +1,33 @@
+package com.example.yj.ui.accompany;
+
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.yj.R;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class AccompanyFragment extends Fragment {
+
+
+    public AccompanyFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_accompany, container, false);
+    }
+
+}
