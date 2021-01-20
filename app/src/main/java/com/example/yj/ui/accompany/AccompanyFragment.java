387a7133ad5f80ab333669503bb73fb7d89be826
15507 <1550751448@qq.com>
2021-01-20 16:37:03 +0800

    accompany banner
    陪伴的轮播图

--- app/src/main/java/com/example/yj/ui/accompany/AccompanyFragment.java
+++ app/src/main/java/com/example/yj/ui/accompany/AccompanyFragment.java
@@ -1,22 +1,39 @@
 package com.example.yj.ui.accompany;
 
 
+import android.content.Context;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
+import com.bumptech.glide.Glide;
 import com.example.yj.R;
+import com.youth.banner.Banner;
+import com.youth.banner.BannerConfig;
+import com.youth.banner.Transformer;
+
+import com.youth.banner.listener.OnBannerListener;
+import com.youth.banner.loader.ImageLoader;
+
+import java.util.ArrayList;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class AccompanyFragment extends Fragment {
-
+public class AccompanyFragment extends Fragment implements OnBannerListener {
+    //轮播图
+    private Banner banner;
+    private ArrayList<String> list_path;
+    private ArrayList<String> list_title;
 
     public AccompanyFragment() {
         // Required empty public constructor
@@ -30,4 +47,59 @@ public class AccompanyFragment extends Fragment {
         return inflater.inflate(R.layout.fragment_accompany, container, false);
     }
 
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        initBanner(view);
+    }
+
+    private void initBanner( View view) {
+        banner = view.findViewById(R.id.accompany_banner);
+        //放图片地址的集合
+        list_path = new ArrayList<>();
+        //放标题的集合
+        list_title = new ArrayList<>();
+
+        list_path.add("https://s3.ax1x.com/2021/01/20/sWyNvQ.jpg");
+        list_path.add("https://s3.ax1x.com/2021/01/20/sWytgg.jpg");
+        list_path.add("https://s3.ax1x.com/2021/01/20/sWyJC8.jpg");
+        list_path.add("https://s3.ax1x.com/2021/01/20/sWyY8S.jpg");
+        list_title.add("突破少年");
+        list_title.add("森林戏剧夏令营");
+        list_title.add("上海迪士尼");
+        list_title.add("喀斯特学院");
+        //设置内置样式，共有六种可以点入方法内逐一体验使用。
+        banner.setBannerStyle(BannerConfig.CIRCLE_INDICATOR_TITLE_INSIDE);
+        banner.setImageLoader(new AccompanyFragment.MyLoader());
+        //设置图片网址或地址的集合
+        //设置图片网址或地址的集合
+        banner.setImages(list_path);
+        //设置轮播的动画效果，内含多种特效，可点入方法内查找后内逐一体验
+        banner.setBannerAnimation(Transformer.Default);
+        //设置轮播图的标题集合
+        banner.setBannerTitles(list_title);
+        //设置轮播间隔时间
+        banner.setDelayTime(3000);
+        //设置是否为自动轮播，默认是“是”。
+        banner.isAutoPlay(true);
+        //设置指示器的位置，小点点，左中右。
+        banner.setIndicatorGravity(BannerConfig.CENTER)
+                //以上内容都可写成链式布局，这是轮播图的监听。比较重要。方法在下面。
+                .setOnBannerListener(this)
+                //必须最后调用的方法，启动轮播图。
+                .start();
+
+
+    }
+    //轮播图的监听方法
+    public void OnBannerClick(int position) {
+        Log.i("tag", "你点了第"+position+"张轮播图");
+    }
+    //自定义的图片加载器
+    private class MyLoader extends ImageLoader {
+        @Override
+        public void displayImage(Context context, Object path, ImageView imageView) {
+            Glide.with(context).load((String) path).into(imageView);
+        }
+    }
 }
